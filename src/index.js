ba9d8a34175e3c2709ea26498c7e40ec2c7231b3
Hafid Feghouli <roninhaf@gmail.com>
2018-10-19 14:30:34 -0400

    Can I still buy ?
    Added if condition to check  if have enough for one more purchase.

--- src/index.js
+++ src/index.js
@@ -5,33 +5,44 @@ const SPENDING_THRESHOLD = 80;
 const TAX_RATE = 0.08;
 const PHONE_PRICE = 30;
 
+//calculate tax
 function calculating_tax(amt) {
   amt = amt + amt * TAX_RATE;
   return amt;
 }
 
+//formating price
 function format_price(price) {
   price = "$" + price.toFixed(2);
   return price;
 }
 
+//calculating total price
 function total_price() {
   var total_price = PHONE_PRICE + ACCESSORY_PRICE;
   total_price = calculating_tax(total_price);
   return total_price;
 }
 
+//Do you want to buy ? if yes start looping purchases
 function wannabuy() {
   wannabuy = prompt("Up for a purchase today ?");
 
   if (wannabuy != "yes") {
     alert("Ok, have a good day !");
   } else {
+    //Do you have enough on your bank account
     while (bank_account > 0) {
       if (total_price() <= SPENDING_THRESHOLD) {
         console.log(format_price(total_price()));
         bank_account = bank_account - total_price();
         how_many_phones++;
+        if (total_price() > bank_account) {
+          console.log(
+            "You don't have enough on your bank account for another purchase!"
+          );
+          break;
+        }
       } else {
         break;
       }
@@ -43,5 +54,5 @@ function wannabuy() {
     console.log(format_price(bank_account));
   }
 }
-
-//wannabuy();
+//launch program
+wannabuy();
